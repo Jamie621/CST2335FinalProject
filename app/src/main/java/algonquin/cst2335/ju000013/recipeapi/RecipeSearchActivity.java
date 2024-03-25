b1600381c67a30f4db8ab88338d181d1cfd7aab9
Fei Wu <wu000220@algonquinlive.com>
2024-03-24 21:11:32 -0400

    modify some setting, add toobar.

---
+++ app/src/main/java/algonquin/cst2335/ju000013/recipeapi/RecipeSearchActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.ju000013.recipeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import algonquin.cst2335.ju000013.R;
+
+public class RecipeSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_search);
+    }
+}
